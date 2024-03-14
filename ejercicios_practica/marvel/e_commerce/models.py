1bb01ff821d99ae2573e0fdb3b6e07d8c8459dfb
Juan <juanelabayen@gmail.com>
2024-03-14 19:34:46 -0300

    all

--- ejercicios_practica/marvel/e_commerce/models.py
+++ ejercicios_practica/marvel/e_commerce/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # NOTE: Para poder utilizar el modelo "user" que viene por defecto en Django,
 # Debemos importarlo previamente:
-from django.contrib.auth.models import User
+
 
 
 # Create your models here.
@@ -40,4 +41,37 @@ class Comic(models.Model):
         El método __str__ cumple una función parecida a __repr__ en SQL Alchemy, 
         es lo que retorna cuando llamamos al objeto.
         '''
+        return f'{self.id}'
+
+class WishList(models.Model):
+    id = models.BigAutoField(db_column='ID', primary_key=True)
+    user = models.ForeignKey(
+        User,
+        verbose_name='user',
+        on_delete=models.CASCADE,
+        default=1,
+        blank=True
+    )
+    comic = models.ForeignKey(
+        Comic,
+        verbose_name='comic',
+        on_delete=models.CASCADE,
+        default=1,
+        blank=True
+    )
+    favorite = models.BooleanField(verbose_name='favorite', default=False)
+    cart = models.BooleanField(verbose_name='cart', default=False)
+    wished_qty = models.PositiveIntegerField(
+        verbose_name='wished qty', default=0
+    )
+    bought_qty = models.PositiveIntegerField(
+        verbose_name='bought qty', default=0
+    )
+
+    class Meta:
+        db_table = 'e_commerce_wish_list'
+        verbose_name = 'wish list'
+        verbose_name_plural = 'wish lists'
+
+    def __str__(self):
         return f'{self.id}'
--- ejercicios_practica/marvel/e_commerce/views.py
+++ ejercicios_practica/marvel/e_commerce/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from e_commerce.models import Comic
 # Create your views here.
 def hola_mundo(request):    
     datos =  {"mensaje": "hola_mundo", "curso": "python-django"}
     return JsonResponse(datos)
+
+
+
+
+def get_comic_api_view(request):
+    if request.method == 'GET':
+
+        _queryset = Comic.objects.all()
+      
+        print(_queryset)
+
+        _data = list(_queryset.values()) if _queryset.exists() else []
+             
+
+        return JsonResponse(data=_data, safe=False, status=200)
+        
+    else:
+        return JsonResponse(
+            data={"message": "Método HTTP no permitido."},
+            status=405
+        )
